8878e4e15d21ccb17d8f9d32b41bbf8a03a33fdc
galaxita22 <102005122+galaxita22@users.noreply.github.com>
2022-05-21 22:08:48 -0400

    Código reestructurado
    Código reestructurado, base para el resto del avance

---
+++ Proyecto/src/main/java/cl/utalca/alumnos/proyecto/Main.java
@@ -0,0 +1,28 @@
+package cl.utalca.alumnos.proyecto;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 850, 400);
+        stage.setTitle("Calculadora");
+        stage.setScene(scene);
+
+        //Image icon = new Image(String.valueOf(Main.class.getResource("logo.png")));
+        //stage.getIcons().add(icon);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
